f227f6a9f056c7314f6a32d54cadbed7be7c9a87
khadiza21 <bibikhadiza474@gmail.com>
2022-03-24 03:10:56 +0600

    problems

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
-
+//this is the problem
 function App() {
   return (
     <div>
